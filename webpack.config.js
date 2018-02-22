87b77cb863ee5704146349256c8546fa7feb5f28
Moisés Núñez <moises.nunez.mng@gmail.com>
2018-02-22 21:41:01 +0100

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,6 @@ const webpack = require('webpack')
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-module.exports = {
-  cache: true,
-  devtool: 'cheap-module-source-map',
-  entry: {
-    app: __dirname + '/src/App.js',
-  },
-  output: {
-    path: __dirname + '/public/js',
-    filename: 'bundle.js',
-    publicPath: 'public'
-  },
-  plugins: [
-    // PRODUCTION
-    // new webpack.DefinePlugin({
-    //   'process.env': {
-    //     NODE_ENV: JSON.stringify('production')
-    //   }
-    // }),
-    // new webpack.optimize.UglifyJsPlugin(),
-    // END PRODUCTION
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 8080,
-      proxy: 'http://localhost:3000/',
-      notify: true,
-      open: false,
-      files: ['./public/js/*.js', './public/css/*.css']
-    }),
-    new ExtractTextPlugin({
-      filename: '../../public/css/main.css',
-      allChunks: true,
-    })
-  ],
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          cacheDirectory: true,
-          presets: ['es2015', 'react']
-        }
-      },
-      {const webpack = require('webpack')
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-
 
 const config = (env) => {
   let ret = {
@@ -146,16 +98,3 @@ if(env.NODE_ENV == 'development') {
 }
 
 module.exports = config
-
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract({fallback: 'style-loader', use: [
-            // Minify CSS
-            {loader: 'css-loader', options: {minimize: false}},
-            {loader: 'resolve-url-loader'},
-            {loader: 'sass-loader'}
-          ]
-        })
-      }
-    ]
-  }
-}
